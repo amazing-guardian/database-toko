13bf941011858fe99abd263822dff9c4eb66e345
mzaini30 <muhzaini30@gmail.com>
2018-05-30 22:25:24 +0000

    oke

---
+++ trash/javascript/data/transaksi.js
@@ -0,0 +1,126 @@
+transaksi = [{
+	'tanggal': '2018/02/01',
+	'barang': '20 bmx pacific toxic',
+	'modal': 850000,
+	'jual': 950000
+}, {
+	'tanggal': '2018/02/01',
+	'barang': '12 mini copico',
+	'modal': 425000,
+	'jual': 480000
+}, {
+	'tanggal': '2018/02/02',
+	'barang': 'r3 family kecil',
+	'modal': 305000,
+	'jual': 370000
+}, {
+	'tanggal': '2018/02/02',
+	'barang': 'bw family',
+	'modal': 205000,
+	'jual': 230000
+}, {
+	'tanggal': '2018/02/02',
+	'barang': '12 mini copico',
+	'modal': 425000,
+	'jual': 470000
+}, {
+	'tanggal': '2018/02/07',
+	'barang': '20 bmx pacific rotor',
+	'modal': 1145000,
+	'jual': 1250000
+}, {
+	'tanggal': '2018/02/10',
+	'barang': 'bl 20 x 2125',
+	'modal': 37000,
+	'jual': 50000
+}, {
+	'tanggal': '2018/02/10',
+	'barang': 'gir 18',
+	'modal': 18000,
+	'jual': 30000
+}, {
+	'tanggal': '2018/02/10',
+	'barang': 'bd 12',
+	'modal': 10000,
+	'jual': 15000
+}, {
+	'tanggal': '2018/02/10',
+	'barang': 'r3 kecil',
+	'modal': 305000,
+	'jual': 350000
+}, {
+	'tanggal': '2018/02/12',
+	'barang': '12 mini exotic',
+	'modal': 435000,
+	'jual': 500000
+}, {
+	'tanggal': '2018/02/12',
+	'barang': '16 mini copico',
+	'modal': 440000,
+	'jual': 500000
+}, {
+	'tanggal': '2018/02/14',
+	'barang': '12 mini exotic',
+	'modal': 435000,
+	'jual': 480000
+}, {
+	'tanggal': '2018/02/16',
+	'barang': '16 bmx artini',
+	'modal': 470000,
+	'jual': 520000
+}, {
+	'tanggal': '2018/02/16',
+	'barang': 'bd 24',
+	'modal': 15000,
+	'jual': 20000
+}, {
+	'tanggal': '2018/02/16',
+	'barang': '16 bmx elegance',
+	'modal': 470000,
+	'jual': 520000
+}, {
+	'tanggal': '2018/02/17',
+	'barang': 'r3 kecil bagus',
+	'modal': 320000,
+	'jual': 370000
+}, {
+	'tanggal': '2018/02/17',
+	'barang': '16 mini elegance',
+	'modal': 465000,
+	'jual': 530000
+}, {
+	'tanggal': '2018/02/17',
+	'barang': '20 bmx rotor jie yang',
+	'modal': 870000,
+	'jual': 1000000
+}, {
+	'tanggal': '2018/02/17',
+	'barang': '16 bmx exotic',
+	'modal': 470000,
+	'jual': 520000
+}, {
+	'tanggal': '2018/02/18',
+	'barang': 'bd 12',
+	'modal': 10000,
+	'jual': 15000
+}, {
+	'tanggal': '2018/02/18',
+	'barang': 'bd 12',
+	'modal': 10000,
+	'jual': 15000
+}, {
+	'tanggal': '2018/02/18',
+	'barang': 'bl 12',
+	'modal': 24000,
+	'jual': 30000
+}, {
+	'tanggal': '2018/02/18',
+	'barang': 'bl 12',
+	'modal': 24000,
+	'jual': 30000
+}, {
+	'tanggal': '2018/02/18',
+	'barang': 'ampat',
+	'modal': 6000,
+	'jual': 15000
+}];
